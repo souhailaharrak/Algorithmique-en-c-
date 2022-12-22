a7da8bfeef7d946361725c07bff63681981f03f8
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2022-12-22 21:29:52 +0100

    print your name

---
+++ MyfirstAlgo.cpp
@@ -0,0 +1,24 @@
+ 
+#include <iostream>
+using namespace std;
+
+void PrintName(string Name) {
+
+
+	cout << "notre name is " <<Name;
+
+
+}
+
+
+
+int main()
+{
+ 
+	PrintName("souhaila");
+
+
+	return 0;
+}
+
+ 
