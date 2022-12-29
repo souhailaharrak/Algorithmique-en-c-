c33052d7fce86ae1144251e79bd07821c21a9f9a
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2022-12-30 00:03:17 +0100

    Sum

---
+++ P9.cpp
@@ -0,0 +1,53 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+struct notes {
+
+    int num1;
+    int  num2;
+    int num3;
+};
+
+
+
+notes ReaNumber() {
+    notes numbers;
+    cout << "entre  num1" << endl;
+    cin >> numbers.num1;
+    cout << "entre  num" << endl;
+    cin >> numbers.num2;
+    cout << "entre  num3" << endl;
+    cin >> numbers.num3;
+    return numbers;
+}
+
+
+
+float Somme(notes numbers) {
+
+    int sum = numbers.num1 + numbers.num2 + numbers.num3;
+
+    return sum;
+
+}
+
+
+
+
+void PrintNote(int sum) {
+
+    cout << sum;
+
+}
+
+
+
+
+
+
+int main() {
+
+    PrintNote(Somme(ReaNumber()));
+}
