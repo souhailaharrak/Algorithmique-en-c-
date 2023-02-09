a5e56439e39b9224ac6f2a6b3901e47ef3ce75bb
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2023-02-09 02:03:30 +0100

    Print letters from A to  Z

---
+++ P46.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+
+
+void afficherLitre() {
+
+    for (int l = 65; l <= 90; l++) {
+
+        cout << char(l) << endl;
+    };
+}
+
+
+
+
+
+
+int main() {
+    afficherLitre();
+}
